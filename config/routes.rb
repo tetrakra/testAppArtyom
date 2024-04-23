d8d3960b0b1c8bf723616fd5072f76a6e0079608
Nadya Riddick <nadya.riddick@gmail.com>
2024-04-22 17:08:49 -0700

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
